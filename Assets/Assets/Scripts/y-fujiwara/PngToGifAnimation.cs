4f80e7b5d7e4a7bd85aaa9d150fce64e8dd5ee18
y-fujiwara <fuji.ys5234@gmail.com>
2016-12-11 09:15:59 +0900

    アニメーションの追加
    微修正

--- Assets/Assets/Scripts/y-fujiwara/PngToGifAnimation.cs
+++ Assets/Assets/Scripts/y-fujiwara/PngToGifAnimation.cs
@@ -1,24 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+/// <summary>
+/// y-fujiwara
+/// PNG画像アニメーション用クラス
+/// </summary>
 public class PngToGifAnimation : MonoBehaviour {
+
+	/// <summary>
+    /// PNGを貼り付ける先のGameObject
+    /// </summary>
 	public GameObject obj;
+
+	/// <summary>
+    /// 切り替え間隔の秒数
+    /// </summary>
     public float changeFrameSecond;
+
+	/// <summary>
+    /// イメージ格納先フォルダネーム
+    /// </summary>
     public string folderName;
+
+	/// <summary>
+    /// イメージ名のプレフィックス
+	/// 画像の物理名はheadText_1となる
+    /// </summary>
     public string headText;
+
+	/// <summary>
+    /// イメージ枚数
+    /// </summary>
     public int imageLength;
+
+	/// <summary>
+    /// 一枚目の数字
+	/// 0からか1からかなど
+    /// </summary>
     private int firstFrameNum;
+
+	/// <summary>
+    /// 経過時間
+    /// </summary>
     private float dTime;
 
-    // Use this for initialization
-    void Start () {
+	/// <summary>
+    /// 開始メソッド
+	/// メンバの初期化のみ行う
+    /// </summary>
+    private void Start () {
         firstFrameNum = 1;
         dTime = 0.0f;
     }
 
-    // Update is called once per frame
-    void Update () {
+	/// <summary>
+    /// 更新用メソッド
+	/// 毎フレームごとに呼ばれるため,本メソッドでアニメーション処理を行う
+    /// </summary>
+    private void Update () {
         dTime += Time.deltaTime;
         if (changeFrameSecond < dTime) {
             dTime = 0.0f;
@@ -28,4 +69,11 @@ public class PngToGifAnimation : MonoBehaviour {
         Texture tex = Resources.Load(folderName + "/" + headText + firstFrameNum) as Texture;
         obj.GetComponent<Renderer>().material.SetTexture ("_MainTex", tex);
     }
+
+	/// <summary>
+    /// 終了処理
+    /// </summary>
+	private void FInishScene() {
+		SceneManager.LoadScene ("game");
+	}
 }
